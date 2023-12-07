5985dad19b74b3f26f56ab05690040f516b619dd
dorian-sz <dorian.szatmari@gmail.com>
2023-12-07 17:30:29 +0100

    Add taxually http client

---
+++ Taxually.TechnicalTest/Taxually.TechnicalTest/Helpers/Classes/HttpClient/TaxuallyHttpClient.cs
@@ -0,0 +1,14 @@
+﻿using Taxually.TechnicalTest.Helpers.Interfaces.HttpClient;
+using Taxually.TechnicalTest.Models;
+
+namespace Taxually.TechnicalTest.Helpers.Classes.HttpClient
+{
+    public class TaxuallyHttpClient : ITaxuallyHttpClient<VatRegistrationModel>
+    {
+        public Task PostAsync(string url, VatRegistrationModel request)
+        {
+            // Actual HTTP call removed for purposes of this exercise
+            return Task.CompletedTask;
+        }
+    }
+}
