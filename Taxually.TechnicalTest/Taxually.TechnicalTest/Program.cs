f22cf17500e690fb9deabc7b26c85bae1178c041
dorian-sz <dorian.szatmari@gmail.com>
2023-12-07 22:35:54 +0100

    Add dependency injection to services

--- Taxually.TechnicalTest/Taxually.TechnicalTest/Program.cs
+++ Taxually.TechnicalTest/Taxually.TechnicalTest/Program.cs
@@ -1,3 +1,17 @@
+using Taxually.TechnicalTest.Helpers.Classes.CsvBuilder;
+using Taxually.TechnicalTest.Helpers.Classes.HttpClient;
+using Taxually.TechnicalTest.Helpers.Classes.QueueClient;
+using Taxually.TechnicalTest.Helpers.Classes.VatRegistration;
+using Taxually.TechnicalTest.Helpers.Classes.VatRegistrationFactory;
+using Taxually.TechnicalTest.Helpers.Classes.XmlBuilder;
+using Taxually.TechnicalTest.Helpers.Interfaces.CsvBuilder;
+using Taxually.TechnicalTest.Helpers.Interfaces.HttpClient;
+using Taxually.TechnicalTest.Helpers.Interfaces.QueueClient;
+using Taxually.TechnicalTest.Helpers.Interfaces.VatRegistration;
+using Taxually.TechnicalTest.Helpers.Interfaces.VatRegistrationFactory;
+using Taxually.TechnicalTest.Helpers.Interfaces.XmlBuilder;
+using Taxually.TechnicalTest.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +20,16 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<IVatRegistration<VatRegistrationModel>, GbVatRegistration>();
+builder.Services.AddScoped<IVatRegistration<VatRegistrationModel>, FrVatRegistration>();
+builder.Services.AddScoped<IVatRegistration<VatRegistrationModel>, DeVatRegistration>();
+builder.Services.AddScoped<IVatRegistrationFactory<IVatRegistration<VatRegistrationModel>>, VatRegistrationFactory>();
+builder.Services.AddTransient<ITaxuallyHttpClient<VatRegistrationModel>, TaxuallyHttpClient>();
+builder.Services.AddTransient<ITaxuallyQueueClient<byte[]>, CsvTaxuallyQueueClient>();
+builder.Services.AddTransient<ITaxuallyQueueClient<string>, XmlTaxuallyQueueClient>();
+builder.Services.AddTransient<ICsvBuilder, CsvBuilder>();
+builder.Services.AddTransient<IXmlBuilder<VatRegistrationModel>, XmlBuilder>();
 
 var app = builder.Build();
 
