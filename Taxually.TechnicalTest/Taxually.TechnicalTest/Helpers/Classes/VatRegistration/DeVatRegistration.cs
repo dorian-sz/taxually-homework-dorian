49e2d2257fd4abf78e194907de43e2c08d3956b3
dorian-sz <dorian.szatmari@gmail.com>
2023-12-07 19:30:57 +0100

    Add de vat registration

---
+++ Taxually.TechnicalTest/Taxually.TechnicalTest/Helpers/Classes/VatRegistration/DeVatRegistration.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+using System.Xml.Serialization;
+using Taxually.TechnicalTest.Helpers.Interfaces.QueueClient;
+using Taxually.TechnicalTest.Helpers.Interfaces.VatRegistration;
+using Taxually.TechnicalTest.Helpers.Interfaces.XmlBuilder;
+using Taxually.TechnicalTest.Models;
+
+namespace Taxually.TechnicalTest.Helpers.Classes.VatRegistration
+{
+    public class DeVatRegistration : IVatRegistration<VatRegistrationModel>
+    {
+        private readonly ITaxuallyQueueClient<string> _queueClient;
+        private readonly IXmlBuilder<VatRegistrationModel> _xmlBuilder;
+
+        public DeVatRegistration(ITaxuallyQueueClient<string> queueClient, IXmlBuilder<VatRegistrationModel> xmlBuilder)
+        {
+            _queueClient = queueClient;
+            _xmlBuilder = xmlBuilder;
+        }
+
+        public async Task Register(VatRegistrationModel registrationModel)
+        {
+            var xml = _xmlBuilder.BuildXml(registrationModel);
+            // Queue xml doc to be processed
+            await _queueClient.EnqueueAsync("vat-registration-xml", xml);
+        }
+    }
+}
