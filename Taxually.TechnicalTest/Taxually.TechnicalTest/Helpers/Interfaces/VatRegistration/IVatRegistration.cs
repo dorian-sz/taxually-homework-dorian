b7d1709badd930a670d79aca1a3d6276b1cf8f09
dorian-sz <dorian.szatmari@gmail.com>
2023-12-07 11:21:08 +0100

    Add IVatRegistration

---
+++ Taxually.TechnicalTest/Taxually.TechnicalTest/Helpers/Interfaces/VatRegistration/IVatRegistration.cs
@@ -0,0 +1,9 @@
+﻿using Taxually.TechnicalTest.Models;
+
+namespace Taxually.TechnicalTest.Helpers.Interfaces.VatRegistration
+{
+    public interface IVatRegistration <T>
+    {
+        void Register(T registrationModel);
+    }
+}
