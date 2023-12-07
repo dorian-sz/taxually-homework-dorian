7c19088883dc644f78f7b094d34714cb9b227d4f
dorian-sz <dorian.szatmari@gmail.com>
2023-12-07 16:56:53 +0100

    Revert "Add vat registration interface"
    This reverts commit 28dba70fcd5c4e4e5c91320db8a16e79a45abbf0.

--- Taxually.TechnicalTest/Taxually.TechnicalTest/Helpers/Interfaces/VatRegistration/IVatRegistration.cs
+++
@@ -1,9 +0,0 @@
-﻿using Taxually.TechnicalTest.Models;
-
-namespace Taxually.TechnicalTest.Helpers.Interfaces.VatRegistration
-{
-    public interface IVatRegistration<T>
-    {
-        void Register(T registrationModel);
-    }
-}
