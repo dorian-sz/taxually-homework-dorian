b5694c927410e31f8dcb4e59b095fb24e089b215
dorian-sz <dorian.szatmari@gmail.com>
2023-12-07 19:30:38 +0100

    Add csv builder

---
+++ Taxually.TechnicalTest/Taxually.TechnicalTest/Helpers/Classes/CsvBuilder/CsvBuilder.cs
@@ -0,0 +1,18 @@
+﻿using System.Text;
+using Taxually.TechnicalTest.Helpers.Interfaces.CsvBuilder;
+
+namespace Taxually.TechnicalTest.Helpers.Classes.CsvBuilder
+{
+    public class CsvBuilder : ICsvBuilder
+    {
+        public byte[] BuildCsv(string companyName, string companyId)
+        {
+            var stringBuilder = new StringBuilder();
+            var csvBuilder = new StringBuilder();
+            csvBuilder.AppendLine("CompanyName,CompanyId");
+            csvBuilder.AppendLine($"{companyName}{companyId}");
+
+            return Encoding.UTF8.GetBytes(stringBuilder.ToString());
+        }
+    }
+}
