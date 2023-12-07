6af1b9d2b3866e58a360d364c658b0ab2b0f20dc
dorian-sz <dorian.szatmari@gmail.com>
2023-12-07 17:32:16 +0100

    Add xml queue client

---
+++ Taxually.TechnicalTest/Taxually.TechnicalTest/Helpers/Classes/QueueClient/XmlTaxuallyQueueClient.cs
@@ -0,0 +1,13 @@
+﻿using Taxually.TechnicalTest.Helpers.Interfaces.QueueClient;
+
+namespace Taxually.TechnicalTest.Helpers.Classes.QueueClient
+{
+    public class XmlTaxuallyQueueClient : ITaxuallyQueueClient<string>
+    {
+        public Task EnqueueAsync(string queueName, string payload)
+        {
+            // Code to send to message queue removed for brevity
+            return Task.CompletedTask;
+        }
+    }
+}
