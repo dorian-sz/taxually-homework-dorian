36c7a46c1e1ff67bc4b404525cec6cfbcbdfcd37
dorian-sz <dorian.szatmari@gmail.com>
2023-12-07 17:31:34 +0100

    Add csv queue client

---
+++ Taxually.TechnicalTest/Taxually.TechnicalTest/Helpers/Classes/QueueClient/CsvTaxuallyQueueClient.cs
@@ -0,0 +1,13 @@
+﻿using Taxually.TechnicalTest.Helpers.Interfaces.QueueClient;
+
+namespace Taxually.TechnicalTest.Helpers.Classes.QueueClient
+{
+    public class CsvTaxuallyQueueClient : ITaxuallyQueueClient<byte[]>
+    {
+        public Task EnqueueAsync(string queueName, byte[] payload)
+        {
+            // Code to send to message queue removed for brevity
+            return Task.CompletedTask;
+        }
+    }
+}
