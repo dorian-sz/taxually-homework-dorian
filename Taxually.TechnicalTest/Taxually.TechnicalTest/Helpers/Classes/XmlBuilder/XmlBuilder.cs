443fa8ae9f226e22666d7a88eb818ec8791fa909
dorian-sz <dorian.szatmari@gmail.com>
2023-12-07 19:31:17 +0100

    Add xml builder

---
+++ Taxually.TechnicalTest/Taxually.TechnicalTest/Helpers/Classes/XmlBuilder/XmlBuilder.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+using System.Xml.Serialization;
+using Taxually.TechnicalTest.Helpers.Interfaces.XmlBuilder;
+using Taxually.TechnicalTest.Models;
+
+namespace Taxually.TechnicalTest.Helpers.Classes.XmlBuilder
+{
+    public class XmlBuilder : IXmlBuilder<VatRegistrationModel>
+    {
+        public string BuildXml(VatRegistrationModel registrationModel)
+        {
+            using var stringWriter = new StringWriter();
+            var serializer = new XmlSerializer(typeof(VatRegistrationModel));
+            serializer.Serialize(stringWriter, registrationModel);
+            var xml = stringWriter.ToString();
+            return xml;
+        }
+    }
+}
