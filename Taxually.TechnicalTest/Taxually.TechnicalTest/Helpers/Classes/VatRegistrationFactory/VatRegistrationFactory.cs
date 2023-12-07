e732a24f11f5293bd01285b11f9373cf03ec08ed
dorian-sz <dorian.szatmari@gmail.com>
2023-12-07 22:35:24 +0100

    Add nullable return to vat registration factory

---
+++ Taxually.TechnicalTest/Taxually.TechnicalTest/Helpers/Classes/VatRegistrationFactory/VatRegistrationFactory.cs
@@ -0,0 +1,33 @@
+﻿using Taxually.TechnicalTest.Helpers.Classes.VatRegistration;
+using Taxually.TechnicalTest.Helpers.Interfaces.VatRegistration;
+using Taxually.TechnicalTest.Helpers.Interfaces.VatRegistrationFactory;
+using Taxually.TechnicalTest.Models;
+
+namespace Taxually.TechnicalTest.Helpers.Classes.VatRegistrationFactory
+{
+    public class VatRegistrationFactory : IVatRegistrationFactory<IVatRegistration<VatRegistrationModel>>
+    {
+        private readonly IEnumerable<IVatRegistration<VatRegistrationModel>> _vatRegistrations;
+
+        public VatRegistrationFactory(IEnumerable<IVatRegistration<VatRegistrationModel>> vatRegistrations) 
+        {
+            _vatRegistrations = vatRegistrations;
+        }
+
+        public IVatRegistration<VatRegistrationModel>? GetRegistrationInstance(string countryCode)
+        {
+            return countryCode.ToUpper() switch
+            {
+                "GB" => _FindVatRegistration(typeof(GbVatRegistration)),
+                "DE" => _FindVatRegistration(typeof(DeVatRegistration)),
+                "FR" => _FindVatRegistration(typeof(FrVatRegistration)),
+                _ => null
+            };
+        }
+
+        private IVatRegistration<VatRegistrationModel>? _FindVatRegistration(Type type)
+        {
+            return _vatRegistrations.FirstOrDefault(x => x.GetType() == type);
+        }
+    }
+}
---
+++ Taxually.TechnicalTest/Taxually.TechnicalTest/Helpers/Interfaces/VatRegistrationFactory/IVatRegistrationFactory.cs
@@ -0,0 +1,7 @@
+﻿namespace Taxually.TechnicalTest.Helpers.Interfaces.VatRegistrationFactory
+{
+    public interface IVatRegistrationFactory<T>
+    {
+        T? GetRegistrationInstance(string countryCode);
+    }
+}
