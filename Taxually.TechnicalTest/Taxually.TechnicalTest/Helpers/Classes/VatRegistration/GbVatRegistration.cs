ae4713c1b0b0b6355e0477ca3aeac7790d829375
dorian-sz <dorian.szatmari@gmail.com>
2023-12-07 17:04:32 +0100

    Add gb vat registration

---
+++ Taxually.TechnicalTest/Taxually.TechnicalTest/Helpers/Classes/VatRegistration/GbVatRegistration.cs
@@ -0,0 +1,22 @@
+﻿using Taxually.TechnicalTest.Helpers.Interfaces.HttpClient;
+using Taxually.TechnicalTest.Helpers.Interfaces.VatRegistration;
+using Taxually.TechnicalTest.Models;
+
+namespace Taxually.TechnicalTest.Helpers.Classes.VatRegistration
+{
+    public class GbVatRegistration : IVatRegistration<VatRegistrationModel>
+    {
+        private readonly ITaxuallyHttpClient<VatRegistrationModel> _httpClient;
+
+        public GbVatRegistration(ITaxuallyHttpClient<VatRegistrationModel> httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async void Register(VatRegistrationModel registrationModel)
+        {
+            //unsure of PostAsync real body but if it was known I would add some sort of check if registration was successful and return result.
+            await _httpClient.PostAsync("https://api.uktax.gov.uk", registrationModel);
+        }
+    }
+}
