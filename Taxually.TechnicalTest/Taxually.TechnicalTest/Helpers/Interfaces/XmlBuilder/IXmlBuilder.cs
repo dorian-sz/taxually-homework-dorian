fe604300f6d62bd66983fbfffe58177c27bcbc5a
dorian-sz <dorian.szatmari@gmail.com>
2023-12-07 19:31:07 +0100

    Add xml builder interface

---
+++ Taxually.TechnicalTest/Taxually.TechnicalTest/Helpers/Interfaces/XmlBuilder/IXmlBuilder.cs
@@ -0,0 +1,9 @@
+﻿using Taxually.TechnicalTest.Models;
+
+namespace Taxually.TechnicalTest.Helpers.Interfaces.XmlBuilder
+{
+    public interface IXmlBuilder<T>
+    {
+        string BuildXml(T registrationModel);
+    }
+}
