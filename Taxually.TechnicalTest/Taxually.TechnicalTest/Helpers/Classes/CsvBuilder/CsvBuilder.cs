1be4dffa865bcf405128ffc0a464ff6277913569
dorian-sz <dorian.szatmari@gmail.com>
2023-12-08 10:37:04 +0100

    Remove duplicate instantiation of string builder

--- Taxually.TechnicalTest/Taxually.TechnicalTest/Helpers/Classes/CsvBuilder/CsvBuilder.cs
+++ Taxually.TechnicalTest/Taxually.TechnicalTest/Helpers/Classes/CsvBuilder/CsvBuilder.cs
@@ -7,12 +7,11 @@ namespace Taxually.TechnicalTest.Helpers.Classes.CsvBuilder
     {
         public byte[] BuildCsv(string companyName, string companyId)
         {
-            var stringBuilder = new StringBuilder();
             var csvBuilder = new StringBuilder();
             csvBuilder.AppendLine("CompanyName,CompanyId");
             csvBuilder.AppendLine($"{companyName}{companyId}");
 
-            return Encoding.UTF8.GetBytes(stringBuilder.ToString());
+            return Encoding.UTF8.GetBytes(csvBuilder.ToString());
         }
     }
 }
