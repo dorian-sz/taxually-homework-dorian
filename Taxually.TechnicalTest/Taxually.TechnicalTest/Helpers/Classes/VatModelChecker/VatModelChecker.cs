66b0306f2c20bd54ae08eb661a932bc6dbf13c7a
dorian-sz <dorian.szatmari@gmail.com>
2023-12-09 11:58:42 +0100

    Add vat model property checker

---
+++ Taxually.TechnicalTest/Taxually.TechnicalTest/Helpers/Classes/VatModelChecker/VatModelChecker.cs
@@ -0,0 +1,16 @@
+﻿using Taxually.TechnicalTest.Helpers.Interfaces.ModelPropertyChecker;
+using Taxually.TechnicalTest.Models;
+
+namespace Taxually.TechnicalTest.Helpers.Classes.VatModelChecker
+{
+    public class VatModelChecker : IModelPropertyChecker<VatRegistrationModel>
+    {
+        public bool AnyPropertiesDefault(VatRegistrationModel model)
+        {
+            return model.GetType().GetProperties()
+                     .Where(pi => pi.PropertyType == typeof(string))
+                     .Select(pi => pi.GetValue(model) as string)
+                     .Any(value => string.IsNullOrEmpty(value));
+        }
+    }
+}
