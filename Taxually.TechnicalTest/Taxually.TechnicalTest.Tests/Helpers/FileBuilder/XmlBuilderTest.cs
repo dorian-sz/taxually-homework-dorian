f2bdc5c4bedba1a434d889a74f92a0e7c869612c
dorian-sz <dorian.szatmari@gmail.com>
2023-12-10 21:53:35 +0100

    Add unit test for checking valid xml

---
+++ Taxually.TechnicalTest/Taxually.TechnicalTest.Tests/Helpers/FileBuilder/XmlBuilderTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using Taxually.TechnicalTest.Helpers.Classes.FileBuilder;
+using Taxually.TechnicalTest.Models;
+
+namespace Taxually.TechnicalTest.Tests.Helpers.FileBuilder
+{
+    public class XmlBuilderTest
+    {
+        [Fact]
+        public void BuildXml_String_IsValidXml_True()
+        {
+            //Arrange
+            var xmlBuilder = new XmlBuilder();
+            var registrationModel = new VatRegistrationModel();
+            var xmlString = xmlBuilder.BuildXml(registrationModel);
+            var expected = true;
+            //Act
+            var actual = IsValidXml(xmlString);
+
+            //Assert
+            Assert.Equal(expected, actual);
+
+        }
+        private bool IsValidXml(string xmlString)
+        {
+            try
+            {
+                // Attempt to parse the string as XML
+                XDocument.Parse(xmlString);
+                return true;
+            }
+            catch (System.Xml.XmlException)
+            {
+                // Parsing failed, so it's not a valid XML
+                return false;
+            }
+        }
+    }
+}
